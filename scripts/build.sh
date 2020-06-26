8b3c6df924d6bbfb0605ab7d29e1ea44cc01866a
razonyang <razonyang@gmail.com>
2020-06-27 06:42:59 +0800

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,13 @@
+# Removes all binaries.
+rm ./gopkgs*
+
+# Linux amd64
 GOOS=linux GOARCH=amd64 go build -tags sqlite3 -o gopkgs-Linux-amd64-sqlit3
 GOOS=linux GOARCH=amd64 go build -tags mysql -o gopkgs-Linux-amd64-mysql
 GOOS=linux GOARCH=amd64 go build -tags postgres -o gopkgs-Linux-amd64-postgres
 GOOS=linux GOARCH=amd64 go build -tags sqlserver -o gopkgs-Linux-amd64-sqlserver
 
+# Windows amd64
 GOOS=windows GOARCH=amd64 go build -tags sqlite3 -o gopkgs-Windows-amd64-sqlit3
 GOOS=windows GOARCH=amd64 go build -tags mysql -o gopkgs-Windows-amd64-mysql
 GOOS=windows GOARCH=amd64 go build -tags postgres -o gopkgs-Windows-amd64-postgres
