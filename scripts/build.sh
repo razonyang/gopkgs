f3ea996c1fd0cecdabaf8c75e9aaa62988d7ce38
razonyang <razonyang@gmail.com>
2020-06-26 19:21:37 +0800

    Add build commands of SQL Server

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
 GOOS=linux GOARCH=amd64 go build -tags sqlite3 -o gopkgs-Linux-amd64-sqlit3
 GOOS=linux GOARCH=amd64 go build -tags mysql -o gopkgs-Linux-amd64-mysql
 GOOS=linux GOARCH=amd64 go build -tags postgres -o gopkgs-Linux-amd64-postgres
+GOOS=linux GOARCH=amd64 go build -tags sqlserver -o gopkgs-Linux-amd64-sqlserver
 
 GOOS=windows GOARCH=amd64 go build -tags sqlite3 -o gopkgs-Windows-amd64-sqlit3
 GOOS=windows GOARCH=amd64 go build -tags mysql -o gopkgs-Windows-amd64-mysql
 GOOS=windows GOARCH=amd64 go build -tags postgres -o gopkgs-Windows-amd64-postgres
+GOOS=windows GOARCH=amd64 go build -tags sqlserver -o gopkgs-Windows-amd64-sqlserver
