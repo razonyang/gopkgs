edf485519fc19561c1c9a80c773214ec1389d4d2
razonyang <razonyang@gmail.com>
2020-07-30 20:40:15 +0800

    Add report cards to dashboard

--- internal/handlers/dashboard/index.go
+++ internal/handlers/dashboard/index.go
@@ -3,6 +3,7 @@ package dashboard
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"clevergo.tech/authmiddleware"
 	"clevergo.tech/clevergo"
@@ -30,12 +31,22 @@ func (h *Handler) index(c *clevergo.Context) error {
 	if err != nil {
 		return err
 	}
+	dailyReportCard, err := h.getDailyReportCard(ctx, userID)
+	if err != nil {
+		return err
+	}
+	monthlyReportCard, err := h.getMonthlyReportCard(ctx, userID)
+	if err != nil {
+		return err
+	}
 
 	return c.Render(http.StatusOK, "dashboard/index.tmpl", clevergo.Map{
 		"page": web.NewPage("Dashboard"),
 		"cards": []Card{
 			domainCard,
 			packageCard,
+			dailyReportCard,
+			monthlyReportCard,
 		},
 	})
 }
@@ -50,6 +61,27 @@ func (h *Handler) getPackageCard(ctx context.Context, userID string) (card Card,
 	card = NewCard("PACKAGES", "cubes", "success", "/package")
 	err = models.CountPackagesByUser(ctx, h.DB, &card.Count, userID)
 	return
+}
+
+func (h *Handler) getDailyReportCard(ctx context.Context, userID string) (card Card, err error) {
+	card = NewCard("Daily Report", "cloud-download-alt", "secondary", "/report")
+	return card, h.getReport(ctx, &card.Count, userID, time.Now())
+}
+
+func (h *Handler) getMonthlyReportCard(ctx context.Context, userID string) (card Card, err error) {
+	card = NewCard("Daily Report", "cloud-download-alt", "info", "/report")
+	return card, h.getReport(ctx, &card.Count, userID, time.Now().AddDate(0, 0, -29))
+}
+
+func (h *Handler) getReport(ctx context.Context, count *int64, userID string, fromDate time.Time) error {
+	query := `
+SELECT COUNT(1) FROM actions 
+LEFT JOIN packages ON packages.id = actions.package_id
+LEFT JOIN domains ON domains.id = packages.domain_id
+WHERE domains.user_id = ?
+	AND actions.created_at >= ?
+`
+	return h.DB.GetContext(ctx, count, query, userID, fromDate.Format("2006-01-02"))
 }
 
 type Card struct {
