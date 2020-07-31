490607d008a45cfd238744c2a9af492af1c90160
razonyang <razonyang@gmail.com>
2020-08-01 01:31:28 +0800

    Add package columns: homepage and license

--- migrations/20200731150801_v1.3.0.up.sql
+++ migrations/20200731150801_v1.3.0.up.sql
@@ -2,3 +2,5 @@ ALTER TABLE `packages` ADD COLUMN `description` VARCHAR(256) NOT NULL DEFAULT ''
 ALTER TABLE `packages` ADD COLUMN `private` TINYINT NOT NULL DEFAULT 0 AFTER `domain_id`;
 ALTER TABLE `packages` ADD INDEX `idx_packages_private` (`private`);
 ALTER TABLE `packages` MODIFY COLUMN `docs` VARCHAR(256) NOT NULL DEFAULT '';
+ALTER TABLE `packages` ADD COLUMN `homepage` VARCHAR(256) NOT NULL DEFAULT '' AFTER `description`;
+ALTER TABLE `packages` ADD COLUMN `license` VARCHAR(256) NOT NULL DEFAULT '' AFTER `homepage`;
