7735c018eaacc2b8c080be2b79fc6b76e43ce600
razonyang <razonyang@gmail.com>
2020-08-01 02:04:53 +0800

    Update badge page

--- public/js/badge.js
+++ public/js/badge.js
@@ -1,13 +1,20 @@
 $(function() {
-
+    new ClipboardJS('.btn-copy');
 });
 
 function loadBadge() {
-    console.log(11)
     var form = $('#badgeForm');
-    var interval = form.find('select[name="interval"]').val();
     var package = form.find('input[name="package"]').val();
+    if (package == '') {
+        $('#badgeURL').val('');
+        return;
+    }
+    var interval = form.find('select[name="interval"]').val();
+    var style = form.find('select[name="style"]').val();
     var url = siteURL + "/badges/downloads/"+interval+"/"+package;
-    $('#badgeURL').text(url);
+    if (style != "") {
+        url += "?style="+style;
+    }
+    $('#badgeURL').val(url);
     $('#badgePreview').attr('src', url);
 }
