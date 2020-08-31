9aaae705dc81cac296a551b942d8b2d462ed6659
razonyang <razonyang@gmail.com>
2020-08-31 21:32:06 +0800

    Fix challenge handler

--- internal/handlers/domain/challenge.go
+++ internal/handlers/domain/challenge.go
@@ -4,13 +4,14 @@ import (
 	"net/http"
 
 	"clevergo.tech/clevergo"
+	"pkg.razonyang.com/gopkgs/internal/core"
 	"pkg.razonyang.com/gopkgs/internal/models"
 )
 
 func (h *Handler) challenge(c *clevergo.Context) error {
 	ctx := c.Context()
 	var domain models.Domain
-	if err := models.FindDomainByChallengeTXT(ctx, h.DB, &domain, c.Host(), c.Params.String("token")); err != nil {
+	if err := models.FindDomainByChallengeTXT(ctx, h.DB, &domain, core.GetHost(c), c.Params.String("token")); err != nil {
 		c.Logger().Error(err)
 		return c.NotFound()
 	}
