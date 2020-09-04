cc960a37fd3ef783b6ac2dea6f71cc5502133f8f
razonyang <razonyang@gmail.com>
2020-09-05 00:29:57 +0800

    Add pagination

--- internal/handlers/search/index.go
+++ internal/handlers/search/index.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"clevergo.tech/clevergo"
@@ -14,7 +15,8 @@ import (
 
 type Package struct {
 	models.Package
-	Downloads int64 `json:"downloads" db:"downloads"`
+	Downloads int64  `json:"downloads" db:"downloads"`
+	FullPath  string `json:"full_path" db:"full_path"`
 }
 
 func (h *Handler) index(c *clevergo.Context) error {
@@ -24,32 +26,52 @@ func (h *Handler) index(c *clevergo.Context) error {
 			"page": web.NewPage("Search"),
 		})
 	}
+
+	pageNum, err := strconv.ParseInt(c.DefaultQuery("page", "1"), 10, 64)
+	if err != nil {
+		return err
+	}
+	if pageNum < 1 {
+		pageNum = 1
+	}
+	limit := int64(2)
+
 	ctx := c.Context()
 
-	packages, totalCount, err := h.getPackages(ctx, q)
+	packages, totalCount, err := h.getPackages(ctx, q, pageNum, limit)
 	if err != nil {
 		return err
 	}
 
 	return c.Render(http.StatusOK, "search/result.tmpl", clevergo.Map{
 		"page":       web.NewPage(fmt.Sprintf("%s - %s", q, "Search Result")),
+		"pageNum":    pageNum,
+		"limit":      limit,
 		"q":          q,
 		"packages":   packages,
 		"totalCount": totalCount,
 	})
 }
 
-func (h *Handler) getPackages(ctx context.Context, name string) (packages []Package, count int64, err error) {
+func (h *Handler) getPackages(ctx context.Context, name string, page, limit int64) (packages []Package, count int64, err error) {
 	query := squirrel.Select().From("packages").
+		Columns(
+			"packages.*",
+			`domains.id as "domain.id"`,
+			`domains.name as "domain.name"`,
+			`CONCAT(domains.name, "/", packages.path) as  full_path`,
+			`IFNULL(actions.downloads, 0) as downloads`,
+		).
 		LeftJoin("domains ON domains.id = packages.domain_id").
+		LeftJoin("(SELECT package_id, COUNT(1) as downloads FROM actions GROUP BY package_id) actions ON actions.package_id = packages.id").
 		Where(squirrel.Eq{
 			"packages.private": 0,
 		}).
-		Where(squirrel.Like{
-			"packages.path": fmt.Sprintf("%%%s%%", name),
+		Having(squirrel.Like{
+			"full_path": fmt.Sprintf("%%%s%%", name),
 		})
 
-	countSql, countArgs, err := query.Columns("COUNT(1) as count").ToSql()
+	countSql, countArgs, err := squirrel.Select("COUNT(1) as count").FromSelect(query, "t").ToSql()
 	if err != nil {
 		return
 	}
@@ -58,14 +80,9 @@ func (h *Handler) getPackages(ctx context.Context, name string) (packages []Pack
 	}
 
 	cloneQuery := query.
-		Columns(
-			"packages.*",
-			`domains.id as "domain.id"`,
-			`domains.name as "domain.name"`,
-		).
-		OrderBy("packages.id").
-		Offset(0).
-		Limit(20)
+		OrderBy("actions.downloads DESC", "packages.path ASC", "packages.id ASC").
+		Offset(uint64((page - 1) * limit)).
+		Limit(uint64(limit))
 	sql, args, err := cloneQuery.ToSql()
 	if err != nil {
 		return
